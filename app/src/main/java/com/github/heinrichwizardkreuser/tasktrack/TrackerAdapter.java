6dd44e944bea2a6127e70d909d2bbbbe0b919e11
HeinrichWizardKreuser <hwkreuser@gmail.com>
2021-02-02 22:38:09 +0200

    added play/pause functionality

--- app/src/main/java/com/github/heinrichwizardkreuser/tasktrack/TrackerAdapter.java
+++ app/src/main/java/com/github/heinrichwizardkreuser/tasktrack/TrackerAdapter.java
@@ -9,6 +9,7 @@ import android.view.ViewGroup;
 //import android.widget.ImageView;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.Chronometer;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -16,8 +17,10 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.RequiresApi;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
@@ -63,27 +66,47 @@ public class TrackerAdapter
         public EditText editText;
         //public LinearLayout linearLayout;
         public RelativeLayout relativeLayout;
+        public FloatingActionButton playButton;
+        public boolean paused = true;
+        public PausableChronometer chronometer;
         public ViewHolder(View itemView) {
             super(itemView);
             //this.imageView = (ImageView) itemView.findViewById(R.id.imageView);
             this.editText = (EditText) itemView.findViewById(R.id.editText);
             relativeLayout = (RelativeLayout)itemView.findViewById(
                     R.id.relativeLayout);
+            this.chronometer = (PausableChronometer)itemView.findViewById(R.id.chronometer);
+            this.playButton = (FloatingActionButton)itemView.findViewById(R.id.fab_play);
+            playButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (paused) {
+                        // start playing
+                        chronometer.start();
+                        paused = false;
+                        playButton.setImageDrawable(ContextCompat.getDrawable(view.getContext(), R.drawable.ic_baseline_pause_24));
+
+                    } else {
+                        // pause
+                        chronometer.stop();
+                        paused = true;
+                        playButton.setImageDrawable(ContextCompat.getDrawable(view.getContext(), R.drawable.ic_baseline_play_arrow_24));
+                    }
+                }
+            });
+
 
             //add listener to remove keyboard and cursor on action done
             this.editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
                 @RequiresApi(api = Build.VERSION_CODES.M)
                 @Override
                 public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-
                     //Toast.makeText(v.getContext(), "triggered " + actionId, Toast.LENGTH_LONG).show();
-
                     if (actionId == EditorInfo.IME_ACTION_DONE) {
                         // hide virtual keyboard
                         InputMethodManager imm = (InputMethodManager) v.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
                         imm.hideSoftInputFromWindow(editText.getWindowToken(), 0);
                         editText.clearFocus();
-
                     }
                     return false;
                 }
